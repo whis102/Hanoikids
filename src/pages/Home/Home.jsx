b1957899ba89b4244d5cc131b204831b58df5fdb
whis102 <chicongnguyen.dev@gmail.com>
2024-07-21 15:49:44 +0700

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
-export default function Home(params) {
+import NavBar from "../../components/NavBar/NavBar";
+
+export default function Home() {
   return (
     <>
+      <NavBar></NavBar>
       <h1>Home</h1>
     </>
   );
