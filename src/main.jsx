b61877c6bc856fe0a5f0e2ac48686c28ec4b8b38
whis102 <chicongnguyen.dev@gmail.com>
2024-07-26 20:21:21 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import GlobalStyles from "./components/GlobalStyles/GlobalStyles.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
